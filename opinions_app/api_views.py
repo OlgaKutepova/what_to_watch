c27af25e2f8b5d6a4b9b6568958a454dd46f48f2
Kolleo <kolleo@yandex.ru>
2026-04-10 15:58:21 +0500

    add all def()

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -2,6 +2,7 @@ from flask import jsonify, request
 
 from . import app, db
 from .models import Opinion
+from .views import random_opinion
 
 
 @app.route('/api/opinions/<int:id>/', methods=['GET'])
@@ -19,7 +20,7 @@ def update_opinion(id):
     opinion.source = data.get('source', opinion.source)
     opinion.added_by = data.get('added_by', opinion.added_by)
     db.session.commit()
-    return jsonify({'opinion': opinion.to_dict()}), 200
+    return jsonify({'opinion': opinion.to_dict()}), 201
 
 
 @app.route('/api/opinions/<int:id>/', methods=['DELETE'])
@@ -45,3 +46,9 @@ def add_opinion():
     db.session.add(opinion)
     db.session.commit()
     return jsonify({'opinion': opinion.to_dict()}), 201
+
+
+@app.route('/api/get-random-opinion/', methods=['GET'])
+def get_random_opinion():
+    opinion = random_opinion()
+    return jsonify({'opinion': opinion.to_dict()}), 200
